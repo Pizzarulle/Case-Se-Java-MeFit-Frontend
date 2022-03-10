5ac138e93e3258b9857238a08e3ebdae7c3a008a
Cusatelli <einarvandevelde@gmail.com>
2022-03-10 07:02:26 +0100

    feat(Workout): create Workout component.

---
+++ src/components/workout/Workout.jsx
@@ -0,0 +1,26 @@
+import styles from "./Workout.module.css";
+
+const Workout = ({ workoutData }) => {
+    const { id, name, type, complete, setLink } = workoutData;
+
+    return (
+        <div>
+            <h1>Workout #{ id }</h1>
+            <div className={ styles.workoutContainer }>
+                <div className={ styles.row }>
+                    <div className={ styles.column }>
+                        <div className={ styles.floatRight }>
+                            <p className={ styles.id }>#{ id }</p>
+                            <p className={ styles.complete }>Completed: <b>{ complete }</b></p>
+                        </div>
+                        <h2 className={ styles.name }>{ name }</h2>
+                        <p className={ styles.type }>{ type }</p>
+                        <a href={ setLink } className={ styles.link }>Set Link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Workout;
