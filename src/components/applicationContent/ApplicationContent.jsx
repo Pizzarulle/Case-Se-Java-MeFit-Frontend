44a20cb98d183e4a96f707fd1782f6024fe1f4b6
Andreas Hellström <andreas.hellstrom@se.experis.com>
2022-03-10 15:27:31 +0100

    feat(Programs): created a skeleton component for displaying available programs, new route and navlink added so one can navigate to the component

--- src/components/applicationContent/ApplicationContent.jsx
+++ src/components/applicationContent/ApplicationContent.jsx
@@ -4,6 +4,7 @@ import Test from "../../views/Test";
 import NoMatch from "../../views/NoMatch";
 import styles from "./ApplicationContent.module.css";
 import Exercises from "../../views/Exercises";
+import Programs from "../../views/Programs";
 
 const ApplicationContent = () => {
   return (
@@ -11,6 +12,7 @@ const ApplicationContent = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercises" element={<Exercises/>}/>
+        <Route path="/programs" element={<Programs/>}/>
         <Route path="/test" element={<Test />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
---
+++ src/views/Programs.jsx
@@ -0,0 +1,10 @@
+const Programs = () => {
+    return (
+        <>
+            <h1>Available programs!</h1>
+
+        </>
+    );
+}
+
+export default Programs;
