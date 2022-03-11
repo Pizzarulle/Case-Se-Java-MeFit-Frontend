67a70a62557fc0f3979113fd327e4961f1e4e317
Andreas Hellström <andreas.hellstrom@se.experis.com>
2022-03-11 10:03:12 +0100

    feat(Programs): display a list of available programs. Click on individual programs to show more info

--- src/views/Programs.jsx
+++ src/views/Programs.jsx
@@ -1,10 +1,19 @@
-const Programs = () => {
-    return (
-        <>
-            <h1>Available programs!</h1>
+import { useState } from "react";
+import Program from "../components/program/Program";
 
-        </>
-    );
-}
+const Programs = () => {
+
+  const [programs, setPrograms] = useState(null)
+  return (
+    <>
+      <h1>Available programs!</h1>
+
+      {programs &&
+        programs.map((program) => (
+          <Program key={program.id} programData={program} />
+        ))}
+    </>
+  );
+};
 
 export default Programs;
